7f7404e94527af95b3adc8ff137c93b3a248ac72
Dani <danisuesmith@gmail.com>
2022-06-11 15:12:13 -0700

    commit tree 41653a76975d103b88335639b7a978c10519c8da

---
+++ js/challenge.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", () => {});
+
+function counter() {}
+
+function incrementCounter() {
+  let count = document.getElementById("counter");
+  count.textContent = count.textContent + 1;
+}
